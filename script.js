c86647da463c3bd58b26a6b3f457635125e33aed
fiberson <arokenyt@gmail.com>
2025-11-03 00:01:13 +0300

    yoyo

---
+++ script.js
@@ -0,0 +1,17 @@
+/* 
+Максим, создай функцию createUser(firstName, lastName), которая возвращает объект с методами и полями:
+
+firstName - имя
+
+lastName - фамилия
+
+getFirstName() — возвращает имя.
+
+getLastName() — возвращает фамилию.
+
+getFullName() — возвращает имя и фамилию с пробелом.
+*/
+
+const user = createUser('Илон', 'Маск')
+
+console.log(user.getName()) // Илон
